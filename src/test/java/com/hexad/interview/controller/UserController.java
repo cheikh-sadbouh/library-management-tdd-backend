bcb32fdf4a7f7c2eb8c0923f57f116e750a07dd2
cheikh sadbouh <cheikhsoftdev@gmail.com>
2020-12-03 03:42:11 +0800

    feat: test & implement web-layer
    -test & implement userController
    -configure swagger-UI
    -set server port to 8090

---
+++ src/test/java/com/hexad/interview/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.hexad.interview.controller;
+
+import com.hexad.interview.model.Book;
+import com.hexad.interview.service.IBookService;
+import com.hexad.interview.service.IUserService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/v1")
+@Api( description = "Provide Rest API to Manage Library ")
+public class UserController {
+  @Autowired
+  IUserService userService;
+  @Autowired private IBookService bookService;
+
+  @GetMapping(value = "/libraryBooks")
+  public ResponseEntity<Set<Book>> getLibraryBooks() {
+    System.out.print(bookService
+            .getLibraryBookList().get().size());
+    return bookService
+        .getLibraryBookList()
+        .map(books -> new ResponseEntity<>(books, HttpStatus.OK))
+        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+  }
+
+
+  @GetMapping("/BorrowedBooks")
+  public ResponseEntity<List<Book>> getBorrowedBookList() {
+    return userService
+        .getBorrowedBooks()
+        .map(books -> new ResponseEntity<>(books, HttpStatus.OK))
+        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+  }
+
+
+  @PutMapping("/BorrowBook/{bookId}")
+  public ResponseEntity<String> borrowBook(@PathVariable("bookId") String bookId) {
+    return new ResponseEntity<>(userService.borrowBook(bookId), HttpStatus.OK);
+  }
+
+  @PutMapping("/ReturnBook/{bookId}")
+  public ResponseEntity<String> returnBook(@PathVariable("bookId") String bookId) {
+    return new ResponseEntity<>(userService.returnBook(bookId), HttpStatus.OK);
+  }
+}
---
+++ src/test/java/com/hexad/interview/controller/UserControllerTest.java
@@ -0,0 +1,119 @@
+package com.hexad.interview.controller;
+
+import com.hexad.interview.model.Book;
+import com.hexad.interview.service.IBookService;
+import com.hexad.interview.service.IUserService;
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+  @Autowired private MockMvc mockMvc;
+  @MockBean private IUserService userService;
+  @MockBean private IBookService bookService;
+
+  @Test
+  public void Should_ReturnLibraryBookList_When_LibraryBookListIsNotEmpty() throws Exception {
+    given(bookService.getLibraryBookList())
+        .willReturn(
+            Optional.of(
+                new HashSet<>(
+                    Arrays.asList(
+                        new Book("1", "book1", 20,"",""),
+                        new Book("2", "book2", 33,"",""),
+                        new Book("3", "book3", 31,"",""),
+                        new Book("4", "book4", 16,"","")))));
+    mockMvc
+        .perform(get("/api/v1/libraryBooks/").accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk());
+    Mockito.verify(bookService).getLibraryBookList();
+  }
+
+  @Test
+  public void Should_NoContent_When_LibraryBookListIsEmpty() throws Exception {
+    given(bookService.getLibraryBookList()).willReturn(Optional.empty());
+
+    mockMvc
+        .perform(get("/api/v1/libraryBooks/").accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isNoContent());
+    Mockito.verify(bookService).getLibraryBookList();
+  }
+
+  @Test
+  public void Should_ReturnBorrowedBookList_When_BorrowedBookListIsNotEmpty() throws Exception {
+    given(userService.getBorrowedBooks())
+        .willReturn(Optional.of(new ArrayList<>(Arrays.asList(new Book("1", "book1", 20,"","")))));
+    mockMvc
+        .perform(get("/api/v1/BorrowedBooks/").accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk());
+    Mockito.verify(userService).getBorrowedBooks();
+  }
+
+  @Test
+  public void Should_NoContent_When_BorrowedBookListIsEmpty() throws Exception {
+    given(userService.getBorrowedBooks()).willReturn(Optional.empty());
+    mockMvc
+        .perform(get("/api/v1/BorrowedBooks/").accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isNoContent());
+    Mockito.verify(userService).getBorrowedBooks();
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+    "1,Book Not Found",
+    "1,reached maximum borrow limit",
+    "2,already has a book copy",
+    "3,book 3 has been borrowed"
+  })
+   void Should_BorrowBook_When_BookIsFoundInLibraryBookList(
+      String bookId, String expectedResult) throws Exception {
+    given(userService.borrowBook(bookId)).willReturn(expectedResult);
+    mockMvc
+        .perform(put("/api/v1/BorrowBook/" + bookId).accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk());
+    Mockito.verify(userService).borrowBook(bookId);
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+          "1,Book Not Found",
+          "5,book 5 has been returned"
+  })
+  void Should_ReturnBook_When_BookIsFoundInBorrowedBookList(
+          String bookId, String expectedResult) throws Exception {
+    given(userService.returnBook(bookId)).willReturn(expectedResult);
+    mockMvc
+            .perform(put("/api/v1/ReturnBook/" + bookId).accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk());
+    Mockito.verify(userService).returnBook(bookId);
+  }
+}
